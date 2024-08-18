813ca08a76d4293d972cb6fff79fc83f019cc5db
ChrisH2727 <chris@hullbrook.net>
2024-08-18 09:12:15 +0100

    Initial commit

--- Env86/settings.py
+++ Env86/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts.apps.AccountsConfig",  # new
+    "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig"  # new
 
 ]
 
